bd5c2b326fb38669a669032d1c42194d80d0b3f5
razvanr91 <razvan.roman91@outlook.com>
2021-10-07 15:25:56 +0100

    Initial commit

---
+++ GuessTheButton/wwwroot/js/custom.js
@@ -0,0 +1,47 @@
+﻿let button = document.getElementById("press");
+let buttonsContainer = document.getElementById("buttonsContainer");
+
+button.addEventListener("click", playGame);
+
+const playGame = () => {
+
+};
+
+function generateButtons() {
+    let numberOfButtons = document.getElementById("numberOfButtons").value;
+    let randomGeneratedNumbers = createArrayOfNumbers(numberOfButtons);
+
+    var winningNumber = randomGeneratedNumbers[generateWinningNumber(randomGeneratedNumbers.length - 1)];
+
+    for (let i = 0; i < numberOfButtons; i++) {
+        console.log(`${randomGeneratedNumbers[i]} ${winningNumber}`);
+        let generatedButton = createButton();
+
+        generatedButton.id = `btn-${randomGeneratedNumbers[i]}`;
+
+        buttonsContainer.appendChild(generatedButton);
+    }
+
+}
+
+function createButton() {
+    let newButton = document.createElement("button");
+    newButton.classList.add("btn", "btn-secondary", "my-1", "mx-1");
+    newButton.innerHTML = "Am I the winner?";
+    return newButton;
+}
+
+function generateWinningNumber(enteredNumber) {
+    return Math.floor(Math.random() * enteredNumber + 1);
+}
+
+function createArrayOfNumbers(numberOfButtons) {
+    let numbers = [];
+
+    while (numbers.length < numberOfButtons) {
+        let randomNumber = generateWinningNumber(numberOfButtons * numberOfButtons);
+        if (!numbers.includes(randomNumber)) numbers.push(randomNumber);
+    }
+
+    return numbers;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GuessTheButton
+ The game is simple. Enter how many buttons you want to generate, then try to guess the correct one. But there's a catch. You have only one life, so be carefull!
