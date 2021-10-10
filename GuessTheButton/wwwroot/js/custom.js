677b999b9fe19ed2662d47fb6ad3a7ada8c176b8
razvanr91 <razvan.roman91@outlook.com>
2021-10-10 22:28:14 +0100

    Add function to modify button and to check each button

--- GuessTheButton/wwwroot/js/custom.js
+++ GuessTheButton/wwwroot/js/custom.js
@@ -5,102 +5,94 @@ let numberInput = document.getElementById("numberOfButtons");
 button.addEventListener("click", (e) => generateButtons(e));
 
 function generateButtons(e) {
-  if (buttonsContainer.childElementCount > 0) {
-    window.location.reload();
-  } else {
-    e.preventDefault();
+	if (buttonsContainer.childElementCount > 0) {
+		window.location.reload();
+	} else {
+		e.preventDefault();
 
-    let numberOfButtons = numberInput.value;
+		let numberOfButtons = numberInput.value;
 
-    if (numberOfButtons >= 2) {
-      numberInput.disabled = true;
+		if (numberOfButtons >= 2) {
+			numberInput.disabled = true;
 
-      if (button.classList.contains("btn-danger")) {
-        button.classList.replace("btn-danger", "btn-success");
-      }
+			if (button.classList.contains("btn-danger")) {
+				button.classList.replace("btn-danger", "btn-success");
+			}
 
-      button.classList.replace("btn-primary", "btn-success");
+			button.classList.replace("btn-primary", "btn-success");
 
-      button.innerHTML = "Play again";
+			button.innerHTML = "Play again";
 
-      let randomGeneratedNumbers = createArrayOfNumbers(numberOfButtons);
+			let randomGeneratedNumbers = createArrayOfNumbers(numberOfButtons);
 
-      var winningNumber =
-        randomGeneratedNumbers[
-          generateWinningNumber(randomGeneratedNumbers.length - 1)
-        ];
+			var winningNumber =
+				randomGeneratedNumbers[generateWinningNumber(randomGeneratedNumbers.length - 1)];
 
-      for (let i = 0; i < numberOfButtons; i++) {
-        console.log(`${randomGeneratedNumbers[i]} ${winningNumber}`);
-        let generatedButton = createButton();
+			for (let i = 0; i < numberOfButtons; i++) {
+				let currentIndex = randomGeneratedNumbers[i];
+				let generatedButton = createButton();
 
-        generatedButton.id = `btn-${randomGeneratedNumbers[i]}`;
+				generatedButton.id = `btn-${currentIndex}`;
 
-        buttonsContainer.appendChild(generatedButton);
-      }
+				let isWinner = checkWinner(winningNumber, currentIndex);
 
-      let gameButtons = buttonsContainer.querySelectorAll("button");
+				generatedButton.addEventListener("click", () => {
+					goThroughButtons(gameButtons, generatedButton, isWinner, winningNumber);
+				});
 
-      gameButtons.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          let buttonId = btn.id.split("-");
+				buttonsContainer.appendChild(generatedButton);
+			}
 
-          if (buttonId[1] == winningNumber) {
-            let winningButton = btn;
-            btn.classList.replace("btn-secondary", "btn-success");
-            btn.innerHTML = `Way to go! You won!`;
+			let gameButtons = buttonsContainer.querySelectorAll("button");
+		} else changeButton(button, "You must create at least 2 buttons...", "btn-primary", "btn-danger");
 
-            gameButtons.forEach((losingButton) => {
-              if (losingButton.id !== winningButton.id) {
-                losingButton.classList.replace("btn-secondary", "btn-danger");
-                losingButton.innerHTML = `Wrong one.`;
-                losingButton.disabled = true;
-              }
-            });
-          } else {
-            gameButtons.forEach((losingButton) => {
-              let losingButtonId = losingButton.id.split("-");
-              if (losingButtonId[1] == winningNumber) {
-                losingButton.innerHTML = `This was the winner!`;
-                losingButton.classList.replace("btn-secondary", "btn-success");
-                losingButton.disabled = true;
-              } else {
-                losingButton.classList.replace("btn-secondary", "btn-danger");
-                losingButton.innerHTML = `Wrong one.`;
-                losingButton.disabled = true;
-              }
-              btn.disabled = false;
-            });
-            btn.classList.replace("btn-secondary", "btn-danger");
-            btn.innerHTML = `Wrong one.`;
-          }
-        });
-      });
-    } else {
-      button.classList.replace("btn-primary", "btn-danger");
-      button.innerHTML = "You must create at least 2 buttons...";
-    }
-  }
+	}
+}
+
+function goThroughButtons(buttons, choice, outcome, winningNumber) {
+	buttons.forEach((button) => {
+		let buttonId = button.id.split("-");
+		if (button === choice && outcome) {
+			changeButton(button, "Way to go! You won!", "btn-secondary", "btn-success");
+		} else {
+			if (Number.parseInt(buttonId[1]) === winningNumber) {
+				changeButton(button, "This was the winner!", "btn-secondary", "btn-success");
+        button.disabled = true;
+			} else {
+				changeButton(button, "Wrong one.", "btn-secondary", "btn-danger");
+			}
+		}
+	});
+}
+
+function changeButton(selectedButton, text, classFrom, classTo) {
+	selectedButton.classList.replace(classFrom, classTo);
+	selectedButton.innerHTML = text;
+	return selectedButton;
+}
+
+function checkWinner(winnerId, clickedId) {
+	return winnerId === clickedId;
 }
 
 function createButton() {
-  let newButton = document.createElement("button");
-  newButton.classList.add("btn", "btn-secondary", "my-1", "mx-1", "w-25");
-  newButton.innerHTML = "Am I the winner?";
-  return newButton;
+	let newButton = document.createElement("button");
+	newButton.classList.add("btn", "btn-secondary", "my-1", "mx-1", "w-25");
+	newButton.innerHTML = "Am I the winner?";
+	return newButton;
 }
 
 function generateWinningNumber(enteredNumber) {
-  return Math.floor(Math.random() * enteredNumber + 1);
+	return Math.floor(Math.random() * enteredNumber + 1);
 }
 
 function createArrayOfNumbers(numberOfButtons) {
-  let numbers = [];
+	let numbers = [];
 
-  while (numbers.length < numberOfButtons) {
-    let randomNumber = generateWinningNumber(numberOfButtons * numberOfButtons);
-    if (!numbers.includes(randomNumber)) numbers.push(randomNumber);
-  }
+	while (numbers.length < numberOfButtons) {
+		let randomNumber = generateWinningNumber(numberOfButtons * numberOfButtons);
+		if (!numbers.includes(randomNumber)) numbers.push(randomNumber);
+	}
 
-  return numbers;
+	return numbers;
 }
